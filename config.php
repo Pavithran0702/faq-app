0850010112ff0e46a38b00a101aae583fc1df6e0
Pavithran0702 <pavithranchristino56@gmail.com>
2026-02-28 12:35:40 +0530

    Database connection config

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+$host = "localhost";
+$dbname = "faq_app";
+$username = "root";
+$password = "";
+$port = "3307";
+
+try {
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Database Connection Failed: " . $e->getMessage());
+}
